fc7c968a6ac4857228cf7bbab3f6a172f61c1d66
lydia-atieno <68909742+lydia-atieno@users.noreply.github.com>
2022-04-25 17:36:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector('.header .nav-bar .nav-list .menu-btn');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        mobile_menu.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        mobile_menu.classList.remove('open');
+        menuOpen = false;
+    }
+});
